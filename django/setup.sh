c406fc8a53308eb2b1aea0813c8a566c621522a0
David VanGorder <vangordd@gmail.com>
2019-01-17 07:46:36 -0600

    updating ifs in setup

--- django/setup.sh
+++ django/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Make sure CentOs software collections is in repos.d for newer packages
-if [[ !$(/usr/bin/yum repolist | /usr/bin/grep -i -q 'centos-sclo') ]]; then
+if $/usr/bin/yum repolist | /usr/bin/grep -i -q 'centos-sclo'; then
   echo "CentOS Software Collections already installed"
 else
   echo "Need to install CentOS Software Collections"
@@ -14,10 +14,7 @@ yum install -y -q rh-python35-python
 pip install --upgrade pip
 pip install Django==2.1.5
 
-/usr/bin/grep '. /opt/rh/rh-python35/enable' /home/$(logname)/.bashrc
-ENABLED=$?
-
-if [[ "$ENABLED" != "0" ]]; then
+if ! /usr/bin/grep '. /opt/rh/rh-python35/enable' /home/$(logname)/.bashrc; then
   echo 'Adding Python 3.5 to logged in user path'
   echo -e "\n. /opt/rh/rh-python35/enable" >> /home/$(logname)/.bashrc
 fi
